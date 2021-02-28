9458fc5fd60bd9bbd61a1eff51a01aef4d97c22e
Dan Beers <spanyell@icloud.com>
2021-02-28 13:50:32 -0800

    Added door opening effect to OneSixView.

--- Adventure/View/ChapterOne/OneSixView.swift
+++ Adventure/View/ChapterOne/OneSixView.swift
@@ -13,49 +13,99 @@ struct OneSixView: View {
     @StateObject var storyDataViewModel = StoryDataViewModel()
     @State private var viewAction: Int? = 0
     @State private var viewTransition: Int? = 0
+    @State var doorOpenEffect = true
+    @State var textWalkForward = true
+    @State var blurry = true
+    @State var doorOpenAndCreakSound = try! AVAudioPlayer(data: Constants.doorOpenAndCreak!.data, fileTypeHint: "mp3")
     
     var storyPlacement: Int
     
     var body: some View {
         let choicesArray = storyDataViewModel.choicesDictionary[storyPlacement]
         
-        Text(Constants.ONE_SIX_ONE)
-        Text("\(storyDataViewModel.storyDataList[storyPlacement - 1].dataDescription)")
-            .foregroundColor(.white)
-            .font(Font.custom("Hoefler Text", size: 25))
-        
-        VStack
-        {
-            NavigationLink(
-                destination: OneSevenView(storyPlacement: 7), tag: 1, selection: $viewAction)
-            {
-                EmptyView()
-            }
-            NavigationLink(
-                destination: OneEightView(storyPlacement: 8), tag: 2, selection: $viewAction)
-            {
-                EmptyView()
-            }
-        }
-        Divider().background(Color.white)
-            .frame(height: 100)
-
-        ForEach(choicesArray!.indices, id: \.self)
-        {
-            i in
-
-            Text("\(choicesArray![i])")
+        ZStack {
+            Color.black
+                .frame(alignment: .center)
+            Text("THE\nDOOR\nGROANS\nOPEN.").multilineTextAlignment(.trailing)
                 .foregroundColor(.white)
-                .font(Font.custom("Hoefler Text", size: 20))
-                .padding()
-                .onTapGesture(perform:
-                                {
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0)
-                                    {
-                                        viewAction = i + 1
-                                    }
-                    
-                                })
+                .font(Font.custom("Hoefler Text", size: 70))
+                .rotation3DEffect(
+                    .degrees(doorOpenEffect ? 0 : -76),
+                    axis: (x: 0.0, y: 1.0, z: 0.0),
+                    anchor: .trailing,
+                    anchorZ: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/,
+                    perspective: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/
+                )
+                .scaleEffect(textWalkForward ? 1: 500)
+                .onAppear
+                {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5)
+                    {
+                        withAnimation(Animation.easeInOut(duration: 3))
+                        {
+                            doorOpenEffect.toggle()
+                        }
+                    }
+                    doorOpenAndCreakSound.play()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5)
+                    {
+                        withAnimation(Animation.easeInOut(duration: 3))
+                        {
+                            textWalkForward.toggle()
+                        }
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5)
+                    {
+                        withAnimation(Animation.easeInOut(duration: 3))
+                        {
+                            blurry.toggle()
+                        }
+                    }
+                }
+            VStack {
+                Text(Constants.ONE_SIX_ONE)
+                Text("\(storyDataViewModel.storyDataList[storyPlacement - 1].dataDescription)")
+                    .foregroundColor(.white)
+                    .font(Font.custom("Hoefler Text", size: 25))
+                    .blur(radius: blurry ? 500: 0)
+                
+                VStack
+                {
+                    NavigationLink(
+                        destination: OneSevenView(storyPlacement: 7), tag: 1, selection: $viewAction)
+                    {
+                        EmptyView()
+                    }
+                    NavigationLink(
+                        destination: OneEightView(storyPlacement: 8), tag: 2, selection: $viewAction)
+                    {
+                        EmptyView()
+                    }
+                }
+                Divider().background(Color.white)
+                    .frame(height: 100)
+                    .blur(radius: blurry ? 500: 0)
+
+                ForEach(choicesArray!.indices, id: \.self)
+                {
+                    i in
+
+                    Text("\(choicesArray![i])")
+                        .foregroundColor(.white)
+                        .font(Font.custom("Hoefler Text", size: 20))
+                        .padding()
+                        .blur(radius: blurry ? 500: 0)
+                        .onTapGesture(perform:
+                                        {
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0)
+                                            {
+                                                viewAction = i + 1
+                                            }
+                            
+                                        })
+                }
+                
+            }
         }
         .navigationBarHidden(true)
     }
