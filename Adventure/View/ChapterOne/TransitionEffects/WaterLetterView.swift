9812fbe8cbb0a8e6e317485ec75b2b56bd1c5779
Dan Beers <spanyell@icloud.com>
2021-03-31 23:18:51 -0700

    More effective WaterLetterView effects.

--- Adventure/View/ChapterOne/TransitionEffects/WaterLetterView.swift
+++ Adventure/View/ChapterOne/TransitionEffects/WaterLetterView.swift
@@ -13,42 +13,53 @@ struct WaterLetterView: View
     @State var turnLetters = true
     @State var opacityLetters = true
     @State var growCircle = true
+    @State var shadowFadeLetter = true
+
     var body: some View
     {
         ZStack
         {
             Color(.black)
             Circle().stroke(Color.white)
-// Creates the drop effect with a black circle and a white outline that will grow ontap
-                .frame(width: growCircle ? 0 : 5000, height: growCircle ? 0 : 5000)
+                // Creates the drop effect with a black circle and a white outline that will grow ontap
+                .frame(width: growCircle ? 0 : 4000, height: growCircle ? 0 : 4000)
             VStack
             {
                 HStack
                 {
                     // Move and rotate action on each letter from the ontap
                     Text("H")
-                        .offset(x: moveLetters ? 0 : -40, y: moveLetters ? 0 : -50)
-                        .rotationEffect(.degrees(turnLetters ? 0 : -30))
+                        .offset(x: moveLetters ? 0 : -60, y: moveLetters ? 0 : -90)
+                        .rotationEffect(.degrees(turnLetters ? 0 : -40))
+                        .shadow(color: .white, radius: shadowFadeLetter ? 0 : 4, x: shadowFadeLetter ? 0 : 15, y: shadowFadeLetter ? 0 : 5)
 
                     Text("E")
-                        .offset(x: moveLetters ? 0 : -10, y: moveLetters ? 0 : -40)
+                        .offset(x: moveLetters ? 0 : -10, y: moveLetters ? 0 : -130)
                         .rotationEffect(.degrees(turnLetters ? 0 : -20))
+                        .shadow(color: .white, radius: shadowFadeLetter ? 0 : 4, x: shadowFadeLetter ? 0 : 10, y: shadowFadeLetter ? 0 : 15)
+
                     Text("L")
-                        .offset(x: moveLetters ? 0 : 20, y: moveLetters ? 0 : -50)
+                        .offset(x: moveLetters ? 0 : 60, y: moveLetters ? 0 : -110)
                         .rotationEffect(.degrees(turnLetters ? 0 : -10))
+                        .shadow(color: .white, radius: shadowFadeLetter ? 0 : 4, x: shadowFadeLetter ? 0 : -10, y: shadowFadeLetter ? 0 : 15)
+
                     Text("P")
-                        .offset(x: moveLetters ? 0 : 30, y: moveLetters ? 0 : -50)
-                        .rotationEffect(.degrees(turnLetters ? 0 : 30))
+                        .offset(x: moveLetters ? 0 : 70, y: moveLetters ? 0 : -70)
+                        .rotationEffect(.degrees(turnLetters ? 0 : 40))
+                        .shadow(color: .white, radius: shadowFadeLetter ? 0 : 4, x: shadowFadeLetter ? 0 : -15, y: shadowFadeLetter ? 0 : 5)
                 }
 
                 HStack
                 {
                     Text("M")
-                        .offset(x: moveLetters ? 0 : -40, y: moveLetters ? 0 : 50)
+                        .offset(x: moveLetters ? 0 : -100, y: moveLetters ? 0 : 40)
                         .rotationEffect(.degrees(turnLetters ? 0 : -30))
+                        .shadow(color: .white, radius: shadowFadeLetter ? 0 : 4, x: shadowFadeLetter ? 0 : 15, y: shadowFadeLetter ? 0 : -15)
+
                     Text("E")
-                        .offset(x: moveLetters ? 0 : 40, y: moveLetters ? 0 : 50)
-                        .rotationEffect(.degrees(turnLetters ? 0 : -30))
+                        .offset(x: moveLetters ? 0 : 80, y: moveLetters ? 0 : 50)
+                        .rotationEffect(.degrees(turnLetters ? 0 : 30))
+                        .shadow(color: .white, radius: shadowFadeLetter ? 0 : 4, x: shadowFadeLetter ? 0 : -15, y: shadowFadeLetter ? 0 : -15)
                 }
             }
             .font(Font.custom("Hoefler Text", size: 30))
@@ -70,7 +81,16 @@ struct WaterLetterView: View
                     {
                         moveLetters.toggle()
                         turnLetters.toggle()
+                        shadowFadeLetter.toggle()
+                    }
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5)
+                {
+                    withAnimation(Animation
+                        .easeOut(duration: 3))
+                    {
                         opacityLetters.toggle()
+                        shadowFadeLetter.toggle()
                     }
                 }
             }
